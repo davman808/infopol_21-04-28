afa115e59d8d00efff3af7044763d21f6417a4c2
davman808 <da.seyder@gmail.com>
2021-05-23 17:33:29 +0200

    major changes: polished subpage 1 and index, finished subpage 2, renamed the .js

---
+++ PageOpener.js
@@ -0,0 +1,15 @@
+document.getElementById("hvrbtn1").onclick = function transferToSubpage1() {
+  window.open("subpage1.html")
+}
+document.getElementById("hvrbtn2").onclick = function transferToSubpage2() {
+  window.open("subpage2.html")
+}
+document.getElementById("hvrbtn3").onclick = function transferToSubpage3() {
+  window.open("subpage3.html")
+}
+document.getElementById("hvrbtn4").onclick = function transferToSubpage4() {
+  window.open("subpage4.html")
+}
+document.getElementById("hvrbtn5").onclick = function transferToSubpage5() {
+  window.open("subpage5.html")
+}
